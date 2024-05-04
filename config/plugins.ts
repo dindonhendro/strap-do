94dbdc73e0d0aa0089ca96f3ab93adc715f15aea
dindonhendro <dindon.hendro@gmail.com>
2024-05-04 09:08:44 +0700

    enable content type builder

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default () => ({
+    'content-type-builder': {
+      enabled: true,
+      resolve: './node_modules/@strapi/plugin-content-type-builder'
+    },
+  });
+  
