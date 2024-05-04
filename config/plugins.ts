23114a0d386ce985f44df4b8ee2bfda91e4deb5f
dindonhendro <dindon.hendro@gmail.com>
2024-05-04 12:00:33 +0700

    enable transformer plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,5 +3,35 @@ export default () => ({
       enabled: true,
       resolve: './node_modules/@strapi/plugin-content-type-builder'
     },
+    'transformer': {
+        enabled: true,
+        config: {
+          responseTransforms: {
+            removeAttributesKey: true,
+            removeDataKey: true,
+          },
+          requestTransforms : {
+            wrapBodyWithDataKey: true
+          },
+          hooks: {
+            preResponseTransform : (ctx) => console.log('hello from the preResponseTransform hook!'),
+            postResponseTransform : (ctx) => console.log('hello from the postResponseTransform hook!')
+          },
+          contentTypeFilter: {
+            mode: 'allow',
+            uids: {
+              'api::article.article': true,
+              'api::category.category': {
+                'GET':true,
+              }
+            }
+          },
+          plugins: {
+            ids: {
+              'slugify': true,
+            }
+          }
+        }
+      },
   });
   
